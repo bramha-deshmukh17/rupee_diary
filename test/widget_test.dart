d95c03f049873c25b94868731d4e0085a8a89435
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2025-09-25 08:56:57 +0530

    Fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:finflowa/main.dart';
+import 'package:rupee_diary/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
